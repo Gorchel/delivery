242f7dcc188e62ca923e5a3253e3737f0beed4ed
Олег Штолин <kasatka567@gmail.com>
2017-03-11 21:17:28 +0300

    Добавил админ панель

--- config/app.php
+++ config/app.php
@@ -156,8 +156,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        SleepingOwl\Admin\Providers\SleepingOwlServiceProvider::class,
-
         Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
 
     ],
