27c650e83bdce9d0e1400f2a8a298b379573beb3
Олег Штолин <kasatka567@gmail.com>
2017-03-11 14:58:17 +0300

    Добавил модели и миграции

--- config/app.php
+++ config/app.php
@@ -158,6 +158,8 @@ return [
 
         SleepingOwl\Admin\Providers\SleepingOwlServiceProvider::class,
 
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
+
     ],
 
     /*
@@ -203,6 +205,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
 
     ],
 
